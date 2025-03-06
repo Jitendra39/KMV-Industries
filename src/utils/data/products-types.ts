93f7520a7ad974456485afdc20f15263f5866f25
Jitendra39 <jeetendrachoudhari213@gmail.com>
2025-03-06 18:25:46 +0530

    initial

---
+++ src/utils/data/products-types.ts
@@ -0,0 +1,24 @@
+export const productsTypes = [
+  {
+    id: "1",
+    name: "Car Wash",
+    count: "172",
+  },
+  {
+    id: "2",
+    name: "Shampoo",
+    count: "131",
+  },
+  // {
+  //   id: "3",
+  //   name: "Tank Tops",
+  //   count: "56",
+  // },
+  // {
+  //   id: "4",
+  //   name: "Dress shirts",
+  //   count: "8",
+  // },
+];
+
+export default productsTypes;
---
+++ src/utils/server.ts
@@ -0,0 +1,7 @@
+// const dev = process.env.NODE_ENV !== "production";
+
+// export const server = dev
+//   ? "http://localhost:3000"
+//   : "https://next-ecommerce-front.vercel.app";
+
+export const server = process.env.FRONTEND_URL;
